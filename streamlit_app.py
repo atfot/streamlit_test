ccfc20ade6ef5f8c849e97a7bf2c200ba79b82e1
atfot <104991246+atfot@users.noreply.github.com>
2023-09-18 16:26:42 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ from google.cloud import storage
 from google.oauth2 import service_account
 
 
-credentials_file = st.secrets.connection.gcs # 외부 배포시 꼭, 환경변수로 만들어서 배포
+credentials_file = st.secrets.connection.gcs
 bucket_name = st.secrets['my_bucket'] # 버킷이름
 st.write(st.secrets.connection.gcs)
 button=st.button('load')
@@ -21,26 +21,19 @@ if button:
     with st.spinner('loading..'):
         st.title('')
         st.divider()
-       	# 서비스키 인증 , Google Cloud Storage용 클라이언트 객체를 생성합니다.
-        cd = service_account.Credentials.from_service_account_file(credentials_file) 
+        cd = service_account.Credentials.from_service_account_info(credentials_file) 
         client = storage.Client(credentials=cd)
        
-       	# 지정해놓은 버킷 이름 검색 ('waktaverse')
         bucket = client.bucket(bucket_name)
         blobs = bucket.list_blobs()
        
-       	# CSV 파일만 필터링
         h5_blobs = [blob for blob in blobs if blob.name.endswith('.h5')]
     
-        # CSV files 을 DataFrames 으로
         models = []
         for blob in h5_blobs:
-            h5_data = blob.download_as_string()
-    
-            model = load_model(io.StringIO(h5_data.decode('utf-8')))
-    
-            models.append(model)
-    
+            h5_data = blob.download_as_string()    
+            model = load_model(io.StringIO(h5_data.decode('utf-8')))    
+            models.append(model)    
      
         models=models[0]
         if models:
